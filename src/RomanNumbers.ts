c3c71af84239e99be1faa3f787f4a266fd3c4fb4
Math Klock <math.klock@gmail.com>
2019-07-05 19:24:43 +0200

    Formatting

--- src/RomanNumbers.ts
+++ src/RomanNumbers.ts
@@ -1,16 +1,16 @@
 export class RomanNumbers {
-  static read(roman: string) {
-    if (roman.length === 1) {
-      if (roman === 'I') {
-        return 1;
-      }
-    } else {
-      let result = 0;
-      for (let char of roman) {
-        result += RomanNumbers.read(char)
-      }
-      return result;
+    static read(roman: string) {
+        if (roman.length === 1) {
+            if (roman === 'I') {
+                return 1;
+            }
+        } else {
+            let result = 0;
+            for (let char of roman) {
+                result += RomanNumbers.read(char);
+            }
+            return result;
+        }
+        return 0;
     }
-    return 0;
-  }
 }
